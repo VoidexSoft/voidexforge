19d5b019f0a9f0aa56696ca7b711b752b9cfe3d8
felixngd <danhdat.gamedeveloper@gmail.com>
2025-05-12 17:32:03 +0700

    test docker

--- go.mod
+++ go.mod
@@ -6,11 +6,14 @@ require (
 	github.com/google/uuid v1.5.0
 	github.com/heroiclabs/nakama-common v1.36.0
 	github.com/stretchr/testify v1.10.0
+	go.uber.org/zap v1.27.0
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,25 +1,24 @@
-// Package main is the entry point for the Nakama plugin.
 package main
 
 import (
 	"context"
 	"database/sql"
-	"time"
-
 	"github.com/heroiclabs/nakama-common/runtime"
+	"time"
 )
 
-// InitModule is called by Nakama when the plugin is loaded.
+// noinspection GoUnusedExportedFunction
 func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error {
 	initStart := time.Now()
 
 	logger.Info("Loading Voidexforge Nakama plugin...")
 
-	// Register an example RPC function
-	if err := initializer.RegisterRpc("hello_world", func(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
-		return "{\"message\":\"Hello World!\"}", nil
-	}); err != nil {
-		return err
+	er := initializer.RegisterRpc("hello_world", func(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
+		return `{"message": "Hello, World!"}`, nil
+	})
+	if er != nil {
+		logger.Error("Failed to register hello_world RPC: %v", er)
+		return er
 	}
 
 	logger.Info("Voidexforge Nakama plugin loaded in '%d' msec.", time.Now().Sub(initStart).Milliseconds())
